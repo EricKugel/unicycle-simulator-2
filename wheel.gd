f5157f4abb8164e8ec33d5ff2cbfbf3ff7ce9ec7
EricKugel <erickugel713@gmail.com>
2025-05-20 21:46:09 -0500

    first commit

---
+++ wheel.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+var torque_impulse = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	
+func _process(delta: float):
+	torque_impulse = Input.get_axis("left", "right") * 700000
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	apply_torque_impulse(torque_impulse * delta)
+	apply_central_impulse(Vector2(torque_impulse * delta * 0.005, 0))
+
+	angular_velocity = clampf(angular_velocity, -10, 10)
